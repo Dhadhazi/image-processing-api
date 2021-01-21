017a5072453c348105a432080f6ce839c05b1210
Dhadhazi <dhadhazi@gmail.com>
2021-01-21 11:09:21 +0100

    serves resized images

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import bodyparser from "body-parser";
 import { resizeImage } from "./resize";
 import config from "./config";
 import { fileExist } from "./utils/fileExist";
+import { generateFileName } from "./utils/generateFileName";
 
 const app = express();
 
@@ -42,20 +43,30 @@ app.get("/images/:imageName", async (req, res) => {
   const height = Number(req.query.h) || null;
 
   if (width || height) {
-    // Get the resized image full name
-    // Check if resized image exists
-    //  If exists send back the link
-    // If does not exists, create it and send back the anme
-  }
+    const imageNameWithoutExtension = imageName.split(".")[0];
+    const resizedImageName = generateFileName(
+      imageNameWithoutExtension,
+      width,
+      height
+    );
+    const resizedImagePath = `${config.IMAGES_FOLDER}/${resizedImageName}`;
 
-  const path = `${config.IMAGES_FOLDER}/${imageName}`;
-
-  if (fileExist(path)) {
-    return res.status(200).sendFile(path);
+    if (fileExist(resizedImagePath)) {
+      return res.status(200).sendFile(resizedImagePath);
+    } else {
+      await resizeImage(imageNameWithoutExtension, width, height);
+      return res.status(200).sendFile(resizedImagePath);
+    }
   } else {
-    return res
-      .status(404)
-      .send("Image failed to process or base file does not exists");
+    const path = `${config.IMAGES_FOLDER}/${imageName}`;
+
+    if (fileExist(path)) {
+      return res.status(200).sendFile(path);
+    } else {
+      return res
+        .status(404)
+        .send("Image failed to process or base file does not exists");
+    }
   }
 });
 
